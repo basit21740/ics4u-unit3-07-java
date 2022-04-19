2321c50be2d578b2abbafd19469c0e790c9f6c22
Ubuntu <ubuntu@ip-172-31-26-142.ec2.internal>
2022-04-19 16:23:38 +0000

    done

---
+++ Vehicle.java
@@ -0,0 +1,118 @@
+/*
+* This program is a Vehicle class.
+*
+* @author  Abdul Basit
+* @version 1.0
+* @since   2022-03-03
+*/
+
+/**
+ * Vehicle.
+ */
+public class Vehicle {
+    /**
+    * Speed variable.
+    */
+    private int speed;
+
+    /**
+    * MaxSpeed variable.
+    */
+    private int maxSpeed;
+
+    /**
+    * Colour variable.
+    */
+    private String colour;
+
+    /**
+    * Constructor.
+    *
+    * @param inicialColour colour
+    * @param topSpeed speed
+    */
+    public Vehicle(final String inicialColour, final int topSpeed) {
+        this.colour = inicialColour;
+        this.maxSpeed = topSpeed;
+        this.speed = 0;
+    }
+
+    /**
+    * Getter.
+    *
+    * @return good
+    */
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    /**
+    * Getter.
+    *
+    * @return good
+    */
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+    /**
+    * Getter.
+    *
+    * @return good
+    */
+    public String getColour() {
+        return this.colour;
+    }
+
+    /**
+    * Setter.
+    *
+    * @param newSpeed good
+    */
+    public void setSpeed(final int newSpeed) {
+        this.speed = newSpeed;
+    }
+
+    /**
+    * Setter.
+    *
+    * @param newColour good
+    */
+    public void setColour(final String newColour) {
+        this.colour = newColour;
+    }
+
+    /**
+    * Print all.
+    */
+    public void status() {
+        System.out.println("Speed: " + speed + "\nMaxSpeed: " + maxSpeed
+                           + "\nColour: " + colour);
+    }
+
+    /**
+    * Accelerate.
+    *
+    * @param accPow power of acceleration
+    * @param accTime time of acceleration
+    */
+    public void accelerate(final int accPow, final int accTime) {
+        this.speed = (accPow * accTime) + this.speed;
+        if (this.speed > maxSpeed) {
+            this.speed = maxSpeed;
+        }
+    }
+
+    /**
+    * Breake.
+    *
+    * @param breakPow break power
+    * @param breakTime time of break
+    */
+    public void breake(final int breakPow, final int breakTime) {
+        this.speed = this.speed - (breakPow * breakTime);
+        if (this.speed < 0) {
+            this.speed = 0;
+        }
+    }
+}
